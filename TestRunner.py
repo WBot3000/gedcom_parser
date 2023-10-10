c4befae0aa4d45ab22a14192ebd68099d6da32bd
WBot3000 <wvenusaur@gmail.com>
2023-10-10 12:04:36 -0400

    Fixed TestRunner

--- TestRunner.py
+++ TestRunner.py
@@ -5,15 +5,18 @@ from tests.US01_Tests import US01_Tests as us01
 from tests.US02_Tests import US02_Tests as us02
 from tests.US03_Tests import US03_Tests as us03
 from tests.US07_Tests import US07_Tests as us07
+from tests.US15_Tests import TestFewerThan15Siblings as us15
+from tests.US21_Tests import TestCheckCorrectGenderForRoles as us21
 from tests.US22_Tests import US22_Tests as us22
+from tests.US23_Tests import TestCheckUniqueNamesAndBirthDates as us23
 from tests.US28_Tests import US28_Tests as us28
 from tests.US42_Tests import US42_Tests as us42
 
 #Index of the array is the number of the corresponding user story minus 1
 #Fill this out as test case files are written
 testCaseClassArray = [us01, us02, us03, None, None, None, us07, None, None, None, 
-                      None, None, None, None, None, None, None, None, None, None, 
-                      None, us22, None, None, None, None, None, us28, None, None,
+                      None, None, None, None, us15, None, None, None, None, None, 
+                      us21, us22, us23, None, None, None, None, us28, None, None,
                       None, None, None, None, None, None, None, None, None, None,
                       None, us42]
 
