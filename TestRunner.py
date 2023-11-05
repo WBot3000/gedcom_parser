3a4bcfed963ef432b2b57ac74426762303db2ff6
Sindhu Buggana <107263510+SINDHUBUGGANA@users.noreply.github.com>
2023-11-05 16:05:54 -0500

    Update TestRunner.py

--- TestRunner.py
+++ TestRunner.py
@@ -12,12 +12,14 @@ from tests.US10_Tests import US10_Tests as us10
 from tests.US11_Tests import US11_Tests as us11
 from tests.US12_Tests import US12_Tests as us12
 from tests.US14_Tests import US14_Tests as us14
-from tests.US15_Tests import TestFewerThan15Siblings as us15
+from tests.US15_Tests import US15_Tests as us15
 from tests.US16_Tests import US16_Tests as us16
-from tests.US21_Tests import TestCheckCorrectGenderForRoles as us21
+from tests.US21_Tests import US21_Tests as us21
 from tests.US22_Tests import US22_Tests as us22
-from tests.US23_Tests import TestCheckUniqueNamesAndBirthDates as us23
+from tests.US23_Tests import US23_Tests as us23
 from tests.US28_Tests import US28_Tests as us28
+from tests.US35_Tests import US35_Tests as us35
+from tests.US36_Tests import US36_Tests as us36
 from tests.US42_Tests import US42_Tests as us42
 
 #Index of the array is the number of the corresponding user story minus 1
